dbfb79a960fc9604187251a7dc70772887622afe
Thiago <titiura@gmail.com>
2023-11-28 10:00:26 -0300

    feat: create new branch infra-docker

--- api/clientes/views.py
+++
@@ -1,8 +0,0 @@
-from clientes.serializers import ClientesSerializers
-from rest_framework import viewsets
-from clientes.models import Cliente
-
-class ClienteViewSet(viewsets.ModelViewSet):
-    queryset = Cliente.objects.all()
-    serializer_class = ClientesSerializers
-
