1b581c3bce52ba5847f8bea952fccbcf06698f4b
Thiago <titiura@gmail.com>
2023-11-28 16:36:02 -0300

    fix: lowercase terraform prod

--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -2,9 +2,9 @@ module "prod" {
   source = "../../infra"
   region_aws = "us-west-2"
   
-  ecr_name = "Producao"
-  descricao = "Aplicacao-de-producao"
-  ambiente = "Ambiante-de-producao"
+  ecr_name = "producao"
+  descricao = "aplicacao-de-producao"
+  ambiente = "ambiante-de-producao"
   maquina = "t2.micro"
   max-aplicacao = 5
 }
