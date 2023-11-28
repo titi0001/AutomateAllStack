ff8815b9759e0665c2b1cf81024f0c0fa8d1dcb4
Thiago <titiura@gmail.com>
2023-11-28 16:33:16 -0300

    feat: creat homolog env

--- env/homolog/backend.tf
+++ env/homolog/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-alura-thiago"
-    key = "Prod/terraform.tfstate"
+    key = "homolog/terraform.tfstate"
     region = "us-west-2"
   }
 }
