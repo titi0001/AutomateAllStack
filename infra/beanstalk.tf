9e012bf72cb993e5df8a4879db10d855e2c87a6e
Thiago <titiura@gmail.com>
2023-11-29 11:26:36 -0300

    feat: add resource beanstalk version

--- infra/beanstalk.tf
+++ infra/beanstalk.tf
@@ -26,3 +26,16 @@ resource "aws_elastic_beanstalk_environment" "env_beanstalk" {
     value     = aws_iam_instance_profile.beanstalk_ec2_profile.name
   }
 }
+
+resource "aws_elastic_beanstalk_application_version" "default" {
+  depends_on = [ 
+    aws_elastic_beanstalk_environment.env_beanstalk,
+    aws_elastic_beanstalk_application.aplicacao_beanstalk,
+    aws_s3_bucket_object.docker
+   ]
+  name        = var.ambiente
+  application = var.nome
+  description = var.descricao
+  bucket      = aws_s3_bucket.beanstalk_deploys.id
+  key         = aws_s3_bucket_object.docker.id
+}
