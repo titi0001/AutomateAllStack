6ff1bb66cd9991d8c9eaa2233e2418b0819fdfd0
Thiago <titiura@gmail.com>
2023-11-29 11:27:53 -0300

    fix: resource s3 terraform

--- infra/s3.tf
+++ infra/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "beanstalk_deploys" {
 
 resource "aws_s3_bucket_object" "docker" {
   depends_on = [
-    aws_s3_bucket.aws_s3_bucket.beanstalk_deploys
+    aws_s3_bucket.beanstalk_deploys
   ]
   bucket = "${var.nome-s3}-deploys"
   key    = "${var.nome-s3}.zip"
