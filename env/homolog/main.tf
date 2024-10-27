6649613b0e932c7e773d7c83803b5d68953e9617
Thiago Pereira <titiura@gmail.com>
2024-10-27 15:12:36 -0300

    Feat: update project

--- env/homolog/main.tf
+++ env/homolog/main.tf
@@ -1,10 +1,13 @@
 module "homolog" {
-  source = "../../infra"
+  source     = "../../infra"
   region_aws = "us-west-2"
-  
-  ecr_name = "Homologacao"
-  descricao = "Aplicacao-de-Homologacao"
-  ambiente = "Ambiante-de-Homologacao"
-  maquina = "t2.micro"
+  nome = "var.nome-s3"
+
+  ecr_name      = "Homologacao"
+  descricao     = "Aplicacao-de-Homologacao"
+  ambiente      = "Ambiente-de-Homologacao"
+  maquina       = "t2.micro"
   max-aplicacao = 3
+
+  nome-s3 = "var.nome-s3"
 }
